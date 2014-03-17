30aa2be7ed53b72aeecd9b8cd399bc96cc80fb97
Carl Whittaker <carl359@gmail.com>
2014-03-17 12:02:45 +0000

    Add fake timers

--- rafty.js
+++ rafty.js
@@ -1,34 +1,67 @@
 (function (window) {
-var handlers = [],
+var FPS = 60,
+    ONE_FRAME = 1000 / FPS,
+    handlers = [],
+    start = 864e5,
+    t;
+    RealDate = window.Date,
+    originalPerformance = window.performance,
     originalRaf = window.requestAnimationFrame;
-
 
 function fakeRaf (handler) {
   handlers.push(handler);
 }
 
 function enable () {
+  t = start; // 2nd Jan 1970
   window.requestAnimationFrame = fakeRaf;
+  window.performance = fakePerformance;
+  window.Date = FakeDate;
 }
 
 function disable () {
   handlers.length = 0;
   window.requestAnimationFrame = originalRaf;
+  window.performance = originalPerformance;
+  window.Date = RealDate;
 }
 
 function tick (millis) {
-  var n = Math.floor((millis / 1000) * 60) || 1;
-  for(var i = 0; i < n; i++) handlers.forEach(invoke);
+  millis = millis || ONE_FRAME;
+  var n = Math.round(millis / ONE_FRAME),
+      now = t,
+      stack = handlers.slice(0);
+  handlers.length = 0;
+  for(var i = 0; i < n; i++) {
+    t += ONE_FRAME;
+    stack.forEach(invoke);
+  }
+  t = now + millis;
 }
 
 function invoke (handler) {
-  handler();
+  handler(fakePerformance.now());
 }
 
-window.rafty = {
-  enable: enable,
-  disable: disable,
-  tick: tick
+fakePerformance = {
+  now: function () {
+    return t;
+  },
+  timing: { navigationStart: start }
 };
+
+var FakeDate = function () {
+  if (arguments.length === 0) return new RealDate(FakeDate.now());
+  var args = Array.prototype.slice.call(arguments, 0);
+  var Constructor = RealDate.bind.apply(RealDate, [null].concat(args));
+  return new Constructor();
+};
+
+FakeDate.now = function () {
+  return Math.round(t);
+};
+
+// TODO export for commonjs, amd, etc.
+window.rafty = { enable: enable, disable: disable, tick: tick };
 
 })(window);
--- test/test.js
+++ test/test.js
@@ -1,30 +1,53 @@
-var originalRaf = window.requestAnimationFrame;
+var originalRaf = window.requestAnimationFrame,
+    originalPerformance = window.performance,
+    OriginalDate = window.Date;
 
 describe('rafty', function () {
   describe('.enable', function () {
-    it('replaces requestAnimationFrame', function () {
+    it('suspends time', function () {
       rafty.enable();
-      expect(window.requestAnimationFrame).not.to.equal(originalRaf);
+      expect(Date.now()).to.equal(864e5);
+      expect((new Date()).toString()).to.equal((new OriginalDate(864e5)).toString());
+      expect(performance.now()).to.equal(864e5);
+      expect(performance.timing.navigationStart).to.equal(864e5);
       rafty.disable();
+    });
+
+    it('does not mess with date creation', function () {
+      rafty.enable();
+      expect((new Date(2013, 3, 16)).toString()).to.equal((new OriginalDate(2013, 3, 16)).toString());
     });
   });
 
   describe('.disable', function () {
-    it('restores requestAnimationFrame', function () {
+    beforeEach(function () {
       rafty.enable();
       rafty.disable();
+    });
+
+    it('restores requestAnimationFrame', function () {
       expect(window.requestAnimationFrame).to.equal(originalRaf);
+    });
+
+    it('restores Date', function () {
+      expect(window.Date).to.equal(OriginalDate);
+    });
+
+    it('restores performance', function () {
+      expect(window.performance).to.equal(originalPerformance);
     });
   });
 
   describe('.tick', function () {
-    var i;
+    var i,
+        loop = function () {
+          i++;
+          requestAnimationFrame(loop);
+        };
     beforeEach(function () {
       i = 0;
       rafty.enable();
-      requestAnimationFrame(function () {
-        i++;
-      });
+      requestAnimationFrame(loop);
     });
 
     afterEach(function () {
@@ -36,10 +59,37 @@ describe('rafty', function () {
       expect(i).to.equal(1);
     });
 
+    it('advances time', function () {
+      rafty.tick();
+      expect(Date.now()).to.equal(864e5 + 17);
+      expect(performance.now()).to.equal(864e5 + 16.6666666667);
+      rafty.tick();
+      expect(Date.now()).to.equal(864e5 + 33);
+      expect(performance.now()).to.equal(864e5 + 33.33333334);
+      rafty.tick(500);
+      expect(Date.now()).to.equal(864e5 + 533);
+      expect(performance.now()).to.equal(864e5 + 533.33333334);
+    });
+
+    it('passes performance.now to the handler', function () {
+      var t;
+      requestAnimationFrame(function (now) {
+        t = now;
+      });
+      rafty.tick();
+      expect(t).to.equal(86400016.66666667);
+    });
+
     describe('when given an integer', function () {
       it('plays frames for that number of millis', function () {
         rafty.tick(1000);
         expect(i).to.equal(60);
+      });
+
+      it('advances times', function () {
+        rafty.tick(1000);
+        expect(Date.now()).to.equal(86401000);
+        expect(performance.now()).to.equal(86401000);
       });
     });
   });
